57f3c12c1b583629ea6f09d936fa39310892aff9
Dhruv Rauthan <51470568+dhruvrauthan@users.noreply.github.com>
2022-03-30 01:10:33 +0530

    Updated write.py

--- scripts/write.py
+++ scripts/write.py
@@ -7,7 +7,7 @@ profile = ExecutionProfile(
         consistency_level=ConsistencyLevel.TWO
 )
 
-cluster = Cluster(['192.168.122.11', '192.168.122.12', '192.168.122.13'], execu>
+cluster = Cluster(['192.168.122.11', '192.168.122.12', '192.168.122.13'], execution_profiles={EXEC_PROFILE_DEFAULT: profile})
 session = cluster.connect('edgedb')
 
 print("Connected to Database!")
@@ -27,9 +27,11 @@ for i in data['prizes']:
                 surname = j['surname']
                 year = int(i['year'])
                 category = i['category']
-                query = "INSERT INTO prizes(id, first_name, surname, year, cate>
-                session.execute(query, {'firstname': firstname, 'surname': surn>
+                query = "INSERT INTO prizes(id, first_name, surname, year, category) VALUES (uuid(), %(firstname)s, %(surname)s, %(year)s, %(category)s)"
+                session.execute(query, {'firstname': firstname, 'surname': surname, 'year': year, 'category': category})
 timeEnd = time.time()
 totalTime = (timeEnd - timeStart)*1000
 print("Total time taken : " + str(round(totalTime, 3)) + "ms")
 print("Average Latency : " + str(round(totalTime/945, 3)) + "ms")
+
+
