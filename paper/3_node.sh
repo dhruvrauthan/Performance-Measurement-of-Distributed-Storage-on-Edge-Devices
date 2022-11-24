9cd0b99f14e4c93d35d6b85a262becbb7679277f
Dhruv Rauthan <51470568+dhruvrauthan@users.noreply.github.com>
2022-11-24 10:36:53 +0530

    Create 3_node.sh

---
+++ paper/3_node.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#latencies = "0 10 20 30 40 50 60 70 80 90 100"
+#packetLoss = "0.5 2.5 7.5"
+
+for latency in 10 20 30 40 50 60 70 80 90 100
+do
+        for packetLoss in 0.5 2.5 7.5
+        do
+                ssh edge0@192.168.122.11 -i /home/f20190095/.ssh/id_rsa_benchmark "tcdel --all ens2; 
+                tcset --add ens2 --delay 10ms --network 192.168.122.16; 
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.12;
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.13;exit;"
+
+                ssh edge1@192.168.122.12 -i /home/f20190095/.ssh/id_rsa_benchmark "tcdel --all ens2;
+                tcset --add ens2 --delay 10ms --network 192.168.122.16;
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.13;
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.11;exit;"
+
+                ssh edge2@192.168.122.13 -i /home/f20190095/.ssh/id_rsa_benchmark "tcdel --all ens2;
+                tcset --add ens2 --delay 10ms --network 192.168.122.16; 
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.11;
+                tcset --add ens2 --delay $latency --loss $packetLoss --network 192.168.122.12;exit;"
+
+                ssh endpoint0@192.168.122.16 -i /home/f20190095/.ssh/id_rsa_benchmark "echo \$(python3 write.py) >> 3_node_rf3.txt; exit;"
+        done
+        ssh endpoint0@192.168.122.16 -i /home/f20190095/.ssh/id_rsa_benchmark "echo >> 3_node_rf3.txt; exit;"
+done
