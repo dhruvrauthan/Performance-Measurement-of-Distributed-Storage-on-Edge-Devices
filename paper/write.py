568395aefdd8c928509ac7bc539faa80dd44cef0
Dhruv Rauthan <51470568+dhruvrauthan@users.noreply.github.com>
2022-11-24 10:51:57 +0530

    Create write.py

---
+++ paper/write.py
@@ -0,0 +1,32 @@
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra import ConsistencyLevel
+import json
+import time
+
+profile = ExecutionProfile(
+        consistency_level=ConsistencyLevel.QUORUM
+)
+
+cluster = Cluster(['192.168.122.11', '192.168.122.12', '192.168.122.13', '192.168.122.14', '192.168.122.15'], execution_profiles={EXEC_PROFILE_DEFAULT: profile})
+session = cluster.connect('edgedb')
+
+f = open('sampledata')
+data = json.load(f)
+
+timeStart = time.time()
+for i in data['prizes']:
+        if not i.__contains__('laureates'):
+                continue
+        for j in i['laureates']:
+                firstname = j['firstname']
+                if not j.__contains__('surname'):
+                        continue
+                surname = j['surname']
+                year = int(i['year'])
+                category = i['category']
+                query = "INSERT INTO prizes(id, first_name, surname, year, category) VALUES (uuid(), %(firstname)s, %(surname)s, %(year)s, %(category)s)"
+                session.execute(query, {'firstname': firstname, 'surname': surname, 'year': year, 'category': category})
+timeEnd = time.time()
+totalTime = (timeEnd - timeStart)*1000
+print(str(round(totalTime/1000, 3)) + "ms")
+
