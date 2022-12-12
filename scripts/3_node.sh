43c42f8825a82fd781b2df91e1f41f4e12906c08
Dhruv Rauthan <51470568+dhruvrauthan@users.noreply.github.com>
2022-12-12 20:19:02 +0530

    Added comments

--- scripts/3_node.sh
+++ scripts/3_node.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#latencies = "0 10 20 30 40 50 60 70 80 90 100"
-#packetLoss = "0.5 2.5 7.5"
 
+# Loop for all latencies up to 100 (in increments of 10)
 for latency in 10 20 30 40 50 60 70 80 90 100
 do
+        # Loop for 3 different packet losses
         for packetLoss in 0.5 2.5 7.5
         do
+                # SSH into the edge VM and add the network latency and packet loss
                 ssh edge0@$E0 -i ~/.ssh/id_rsa_benchmark "tcdel --all ens2; 
                 tcset --add ens2 --delay 10ms --network $EP; 
                 tcset --add ens2 --delay $latency --loss $packetLoss --network $E1;
@@ -21,7 +22,7 @@ do
                 tcset --add ens2 --delay $latency --loss $packetLoss --network $E0;
                 tcset --add ens2 --delay $latency --loss $packetLoss --network $E1;exit;"
 
+                # SSH into the endpoint VM and write the results into a file
                 ssh endpoint0@$EP -i ~/.ssh/id_rsa_benchmark "echo \$(python3 write.py) >> 3_node_rf3.txt; exit;"
-        done
-        ssh endpoint0@$EP -i ~/.ssh/id_rsa_benchmark "echo >> 3_node_rf3.txt; exit;"
+        done    
 done
