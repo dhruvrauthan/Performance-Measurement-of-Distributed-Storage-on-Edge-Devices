8b5e56cded847c5c99cb5198519b5273b28b928b
Dhruv Rauthan <51470568+dhruvrauthan@users.noreply.github.com>
2022-12-12 20:40:30 +0530

    Added comments

--- scripts/write.py
+++ scripts/write.py
@@ -3,17 +3,25 @@ from cassandra import ConsistencyLevel
 import json
 import time
 
+# Set the Cassandra consistency level
 profile = ExecutionProfile(
         consistency_level=ConsistencyLevel.QUORUM
 )
 
+# Add all the edge node IP addresses into the cluster
 cluster = Cluster(['192.168.122.11', '192.168.122.12', '192.168.122.13', '192.168.122.14', '192.168.122.15'], execution_profiles={EXEC_PROFILE_DEFAULT: profile})
+
+# Connect to the database
 session = cluster.connect('edgedb')
 
-f = open('sampledata')
+# Open and load the dataset
+f = open('dataset')
 data = json.load(f)
 
+# Start the timer
 timeStart = time.time()
+
+# Extracting and parsing the data from the dataset
 for i in data['prizes']:
         if not i.__contains__('laureates'):
                 continue
@@ -24,8 +32,12 @@ for i in data['prizes']:
                 surname = j['surname']
                 year = int(i['year'])
                 category = i['category']
+                
+                # Creating and executing the query
                 query = "INSERT INTO prizes(id, first_name, surname, year, category) VALUES (uuid(), %(firstname)s, %(surname)s, %(year)s, %(category)s)"
                 session.execute(query, {'firstname': firstname, 'surname': surname, 'year': year, 'category': category})
+                
+# Stop the timer and print the total time
 timeEnd = time.time()
 totalTime = (timeEnd - timeStart)*1000
 print(str(round(totalTime/1000, 3)) + "ms")
